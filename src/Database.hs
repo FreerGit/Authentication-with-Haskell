7ae5f913466ff6d1def053f3d96c1a440f0b131d
FreerGit <fredrik-98@live.se>
2021-02-04 22:09:29 +0100

    feat: GET endpoint for all users
    learned about ToJSON keyValueEntityToJSON and selectList, pretty neat

--- src/Database.hs
+++ src/Database.hs
@@ -4,9 +4,9 @@ module Database where
 
 import           Control.Monad.Logger (runStdoutLoggingT, MonadLogger, LoggingT)
 import           Control.Monad.Reader (runReaderT)
-import           Control.Monad.IO.Class (MonadIO)
+import           Control.Monad.IO.Class (MonadIO, liftIO)
 import           Data.Int (Int64)
-import           Database.Persist (get, insertBy, delete)
+import           Database.Persist (get, insertBy, delete, selectList, Filter, Entity)
 import           Database.Persist.Sql (fromSqlKey, toSqlKey)
 import           Database.Persist.Postgresql (Entity, ConnectionString, withPostgresqlConn, 
                                              runMigration, SqlPersistT)
@@ -25,6 +25,12 @@ migrateDB connString = runAction connString (runMigration migrateAll)
 
 fetchUserDB :: ConnectionString -> Int64 -> IO (Maybe User)
 fetchUserDB connString uid = runAction connString (get (toSqlKey uid))
+
+fetchUsersDB :: ConnectionString -> IO [Entity User]
+fetchUsersDB connString = do
+    found <- runAction connString (selectList ([] :: [Filter User]) [])
+    return found
+
 
 createUserDB :: ConnectionString -> User -> IO (Maybe Int64)
 createUserDB connString user = do
