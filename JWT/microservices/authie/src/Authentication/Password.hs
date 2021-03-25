4b361e6340c2567d9c03d25b52ffd1b9d90928d6
FreerGit <fredrik-98@live.se>
2021-03-25 14:44:32 +0100

    [authie, docs]: some docs on passwords, backlog

--- JWT/microservices/authie/src/Authentication/Password.hs
+++ JWT/microservices/authie/src/Authentication/Password.hs
@@ -12,11 +12,15 @@ bs2t = T.pack . BS.unpack
 t2bs :: T.Text -> BS.ByteString
 t2bs = BS.pack . T.unpack
 
+-- For registration, i would really like to changes the types for this
+-- function. Such as unsafePassowrd -> Password
 hashPassword :: T.Text -> IO T.Text
 hashPassword = fmap (bs2t . fromJust)
     . hashPasswordUsingPolicy fastBcryptHashingPolicy
     . t2bs
 
+-- For registration, i would really like to changes the types for this
+-- function. Such as Password -> unsafePassword -> Bool
 validateHashedPassword :: T.Text -> T.Text -> Bool
 validateHashedPassword hashedPassword originalPassword =
   validatePassword (t2bs hashedPassword) (t2bs originalPassword)
