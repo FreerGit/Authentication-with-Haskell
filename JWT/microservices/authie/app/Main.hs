e7448a83d5d86114dc9774489a0eb93152fcdb50
FreerGit <fredrik-98@live.se>
2021-04-20 21:27:25 +0200

    feat: authie server in docker-compose, SHEEEESH

--- JWT/microservices/authie/app/Main.hs
+++ JWT/microservices/authie/app/Main.hs
@@ -1,17 +1,18 @@
 module Main where
 
-import Control.Monad.Trans.Except (runExceptT)
-
-import Database.User
 import API (runServer)
 import Config
+import Control.Monad.Trans.Except (runExceptT)
+import Database.User
 
 setupDB :: IO ()
 setupDB = do
-    env <- runExceptT initialize
-    case env of
-        Left err -> putStrLn err
-        Right res -> migrateDB (getConnString res)
+  env <- runExceptT initialize
+  case env of
+    Left err -> putStrLn err
+    Right res -> migrateDB (getConnString res)
 
 main :: IO ()
-main = runServer
+main = do
+  setupDB
+  runServer
